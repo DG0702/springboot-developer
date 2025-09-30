d48cdb853cf624d1ed6d83b3d0ff1748d4fec62b
DongGeun <kk506kk@naver.com>
2025-09-30 16:23:37 +0900

    test : 단위 테스트, 통합테스트 코드 작성

---
+++ src/test/java/com/dia/springbootdeveloper/junit/JUnitQuiz.java
@@ -0,0 +1,35 @@
+package com.dia.springbootdeveloper.junit;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitQuiz {
+    
+    @Test
+    public void junitTest(){
+        String name1 = "홍길동";
+        String name2 = "홍길동";
+        String name3 = "홍길은";
+        
+        assertThat(name1).isNotNull();
+        assertThat(name2).isNotNull();
+        assertThat(name3).isNotNull();
+        
+        assertThat(name1).isEqualTo(name2);
+        assertThat(name2).isNotEqualTo(name3);
+    }
+    
+    @Test
+    public void junitTest2(){
+        int number1 = 15;
+        int number2 = 0;
+        int number3 = -15;
+        
+        assertThat(number1).isPositive();
+        assertThat(number2).isZero();
+        assertThat(number3).isNegative();
+        assertThat(number1).isGreaterThan(number2);
+        assertThat(number3).isLessThan(number2);
+    }
+}
