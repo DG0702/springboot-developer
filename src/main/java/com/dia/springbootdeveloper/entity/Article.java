bbb91e31bc299b3adb10fa9d079a296f447bfc1d
DongGeun <kk506kk@naver.com>
2025-10-06 22:12:31 +0900

    feat : 엔티티 컬럼 추가, dto 추가

--- src/main/java/com/dia/springbootdeveloper/entity/Article.java
+++ src/main/java/com/dia/springbootdeveloper/entity/Article.java
@@ -1,7 +1,14 @@
 package com.dia.springbootdeveloper.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -31,6 +39,14 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     public void update(String title, String content){
         this.title = title;
